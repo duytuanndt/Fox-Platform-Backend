99fdd07a94c56724fadc74c99448514f9355d59c
duytuanndt <tuannd@foxcode.org>
2026-04-22 11:50:02 +0700

    feat(init): pm2 configs

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [
+    {
+      name: 'fox-platform-api',
+      script: 'dist/main.js',
+      cwd: '/home/deploy/group-server/Fox-Platform-Backend',
+      instances: 1,
+      exec_mode: 'fork',
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '500M',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3668,
+        ENABLE_I18N: 'false',
+        ENABLE_DATABASE: 'false',
+      },
+    },
+  ],
+};
